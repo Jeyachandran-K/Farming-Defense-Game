72b4f844d2d49cde7ffbbae3c0fa4b3e214fde23
Jeyachandran <jeyachandran.work@gmail.com>
2025-12-30 16:50:25 +0530

    Understanding Character controller

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,47 @@
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField] private float playerMovementSpeed;
+    [SerializeField] private Transform cameraPivot;
+    [SerializeField] private float sensitivity;
+
+    private CharacterController playerCharacterController;
+    private InputSystem_Actions input;
+    private Vector2 keyboardInput;
+    private Vector2 mouseInput;
+    private float mouseInputX;
+
+    
+
+    private void Awake()
+    {
+        playerCharacterController = GetComponent<CharacterController>();
+        input = new InputSystem_Actions();
+    }
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+    private void OnEnable()
+    {
+        input.Enable();
+    }
+    private void OnDisable()
+    {
+        input.Disable();
+    }
+    private void Update()
+    {
+        keyboardInput = input.Player.Move.ReadValue<Vector2>().normalized;
+        mouseInput = input.Player.Look.ReadValue<Vector2>();
+
+         mouseInputX += mouseInput.x*Time.deltaTime*sensitivity;
+        Vector3 move = transform.right * keyboardInput.x + transform.forward * keyboardInput.y;
+
+        playerCharacterController.Move(move*Time.deltaTime*playerMovementSpeed);
+
+        transform.Rotate(Vector3.up * mouseInputX);
+    }
+}
