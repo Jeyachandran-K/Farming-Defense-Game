088a5169c22244542f5302465ed9355b632e26ae
Jeyachandran <jeyachandran.work@gmail.com>
2025-12-31 15:01:55 +0530

    Soil Prefab configured

---
+++ Assets/Scripts/GridManager.cs
@@ -0,0 +1,8 @@
+using Unity.VisualScripting.FullSerializer;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+    //private int width = 50;
+    //private int height = 50;
+}
---
+++ Assets/Scripts/SoilSpawner.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+
+public class SoilSpawner : MonoBehaviour
+{
+    [SerializeField] private Transform parent;
+    [SerializeField] private GameObject soilPrefab;
+
+    private float heightOffset = 0.5f;
+    private void Start()
+    {
+        SpawnSoil();
+    }
+    private void SpawnSoil()
+    {
+        if (soilPrefab == null) return;
+
+        GameObject spawnedSoil = Instantiate(soilPrefab);
+
+        spawnedSoil.transform.SetParent(parent, true);
+        spawnedSoil.transform.localPosition = new Vector3(0, heightOffset,0);
+    }
+}
